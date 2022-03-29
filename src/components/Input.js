e055e74c6e5cae2e97751eaedeb8aa9c09474f6a
rintosaputro <saputrorinto@gmail.com>
2022-03-30 06:27:44 +0700

    feat: add signup screen

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,14 @@
 import {TextInput, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
 
-const Input = ({placeholder}) => {
+const Input = ({placeholder, ...set}) => {
   return (
     <SafeAreaView>
       <TextInput
         placeholder={placeholder}
         placeholderTextColor="#fff"
         style={styles.input}
+        {...set}
       />
     </SafeAreaView>
   );
@@ -16,7 +17,8 @@ const Input = ({placeholder}) => {
 const styles = StyleSheet.create({
   input: {
     height: 60,
-    backgroundColor: 'rgba(255, 255, 255,0.2)',
+    color: '#fff',
+    backgroundColor: 'rgba(178, 190, 195,0.5)',
     borderRadius: 10,
     fontSize: 20,
     paddingHorizontal: 15,
--- src/img/index.js
+++
@@ -1,1 +0,0 @@
-export const BGAUTH = require('./bgAuth.jpg');
