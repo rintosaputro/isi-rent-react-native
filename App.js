f593668564ad48c3f1c3b34a7fb6b60166fcddc3
rintosaputro <saputrorinto@gmail.com>
2022-04-01 06:36:46 +0700

    feat: add search list

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import {NativeBaseProvider} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStackNav from './src/navigation/MainStackNav';
 import AuthStackNav from './src/navigation/AuthStackNav';
-import DetailCategory from './src/screens/DetailCategory';
+import SearchList from './src/screens/SearchList';
 
 const App = () => {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
         {/* <AuthStackNav /> */}
-        <MainStackNav />
+        <SearchList />
       </NativeBaseProvider>
     </NavigationContainer>
   );
