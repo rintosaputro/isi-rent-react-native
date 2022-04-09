c5d79065648a047c613847f923d84558c106a896
rintosaputro <saputrorinto@gmail.com>
2022-04-09 10:29:42 +0700

    fix: conditional search redux

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -89,7 +89,8 @@ const Home = ({navigation}) => {
   };
 
   const handleSearch = () => {
-    dispatch(getFilter(key));
+    const dataFilter = {search: key};
+    dispatch(getFilter(dataFilter));
     navigation.navigate('SearchList');
   };
 
