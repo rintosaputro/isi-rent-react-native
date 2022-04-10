abb9ccc548a0ea7061a25e61980a23083281eb4d
rintosaputro <saputrorinto@gmail.com>
2022-04-11 04:19:20 +0700

    feat: add bootsplash

--- android/app/src/main/java/com/rinto/isirent/MainActivity.java
+++ android/app/src/main/java/com/rinto/isirent/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rinto.isirent;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 
 public class MainActivity extends ReactActivity {
@@ -18,4 +20,26 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "IsiRent";
   }
+  
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        super.loadApp(appKey);
+      }
+    };
+  }
+  // public static class MainActivityDelegate extends ReactActivityDelegate {
+
+  //   // …
+
+  //   @Override
+  //   protected void loadApp(String appKey) {
+  //     RNBootSplash.init(getPlainActivity()); // <- initialize the splash screen
+  //     super.loadApp(appKey);
+  //   }
+  // }
 }
